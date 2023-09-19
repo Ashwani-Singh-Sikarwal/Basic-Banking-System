c88349b9f93757aab6706629ecaffab6830d53c3
Ashwani Singh Sikarwal <ashwanisinghsikarwal0@gmail.com>
2023-09-19 17:08:21 +0530

    File Uploaded

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
-
+let myBalance = parseInt(document.getElementById("myAccountBalance").innerText);
 function sendMoney(){
-   var enterName = document.getElementById("enterName").value;
+   var enterName = document.getElementById("enterName").value.toLowerCase();
    var enterAmount = parseInt(document.getElementById("enterAmount").value);
-
    if (enterAmount > 50000) {
-      alert("Insufficient Balance.")
-   } else {
-      var findUserBankAccount = enterName + "BankBalance";
-      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
-      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
-      document.getElementById("myAccountBalance").innerText = myAccountBalance
-      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
-      alert(`Successful Transaction !! The Amount of $${enterAmount} is sent to ${enterName} Successfully`)
 
-      // transaction history 
-      var createPTag = document.createElement("li");
-      var textNode = document.createTextNode(`$${enterAmount} is sent to Recepient ${enterName.toUpperCase()} on ${Date()}.`);
-      createPTag.appendChild(textNode);
-      var element = document.getElementById("transaction-history-body");
-      element.insertBefore(createPTag, element.firstChild);
+      alert("Insufficient Balance.")
+      
+   } else {
+            var findUserBank = enterName + "BankBalance";
+            var finalAmount = parseInt(document.getElementById(findUserBank).innerHTML) + enterAmount;
+            var myBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+            document.getElementById("myAccountBalance").innerText = myBalance
+            document.getElementById(findUserBank).innerHTML = finalAmount;
+            alert(`Successful Transaction !! The Amount of $${enterAmount} is sent to ${enterName} Successfully`)
+            var createPTag = document.createElement("li");
+            var textNode = document.createTextNode(`$${enterAmount} is sent to Recepient ${enterName.toUpperCase()} on ${Date()}.`);
+            createPTag.appendChild(textNode);
+            var element = document.getElementById("transaction-history");
+            element.insertBefore(createPTag, element.firstChild);
    }
 }
 
